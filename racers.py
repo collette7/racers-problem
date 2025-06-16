f5f08734c606aa345d9ee657d2c5dcc9e14fc3a6
Collette <cece554@gmail.com>
2025-06-16 08:31:34 -0500

    interview prep

--- racers.py
+++ racers.py
@@ -1,6 +1,27 @@
+"""
+1. Set to track drivers who have won at least one race
+2. Set to track all drivers who appear
+3. For each race in the races dictionary:
+- Get the top 3 drivers
+- Add the winner to the winners set
+- Add all 3 drivers to the all_drivers set
+4. Find drivers who are in all_drivers but not in winners
+5. Return the set of losers 
+
+"""
 def non_winners(races):
-    # Write your solution here!
-    pass
+    if not races:
+        return set()
+    
+    winners = set()
+    all_drivers = set()
+    
+    for outcome in races.values():
+        if outcome and len(outcome) > 0:
+            winners.add(outcome[0])
+            all_drivers.update(driver for driver in outcome if driver is not None)
+    
+    return all_drivers - winners
 
 
 races_1 = {
